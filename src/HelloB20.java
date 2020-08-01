752124dc4eb45d30fddf8595a409b58d640d1e6c
aynurerkin <aynurerkin@gmail.com>
2020-08-01 08:50:47 -0700

    updating practice

--- src/HelloB20.java
+++ src/HelloB20.java
@@ -1,5 +1,7 @@
 public class HelloB20 {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("good morning");
+        System.out.println("leaning new things");
     }
 }
