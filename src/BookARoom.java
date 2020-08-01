4e01c7dc85126fd1d5787fb01c77add40a263d73
aynurerkin <64187339+aynurerkin@users.noreply.github.com>
2020-08-01 13:02:03 -0700

    Update BookARoom.java

--- src/BookARoom.java
+++ src/BookARoom.java
@@ -1,5 +1,8 @@
 public class BookARoom {
     public static void main(String[] args) {
         System.out.println("book a room here");
+        
+        System.out.println("would you like a king bed or queen?");
+        
     }
 }
