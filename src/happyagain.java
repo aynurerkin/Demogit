3ac9103959532409d7c782d1f3e4a74043ea766f
aynurerkin <aynurerkin@gmail.com>
2020-08-01 09:34:13 -0700

    Asiya is happy again

---
+++ src/happyagain.java
@@ -0,0 +1,7 @@
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+public class happyagain {
+    public static void main(String[] args) {
+        System.out.println("happy again");
+    }
+}
